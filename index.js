e46f0e6ff919de5077668a675e32c46cb8d4bac0
Jonathan Delgado <jdelgado@rewip.com>
2014-02-15 09:54:33 -0800

    Recoded, redid readme, removed old dependencies

--- index.js
+++ index.js
@@ -1,6 +1,55 @@
+var inq = require('inquirer'),
+	es  = require('event-stream');
+
 module.exports = {
-  confirm: require('./lib/confirm'),
-  input: require('./lib/input'),
-  checkbox: require('./lib/checkbox'),
-  password: require('./lib/password')
+
+	prompt: function(questions, callback){
+
+		return es.map(function(file, cb){
+
+			if(!questions instanceof Array){
+				questions = [questions];
+			}
+
+			if(typeof callback !== 'function'){
+				callback = function(){};
+			}
+
+			inq.prompt(questions, function(res){
+				callback(res);
+				cb(null, file);
+			});
+
+		});
+	},
+
+	confirm: function(options){
+
+		return es.map(function(file, cb){
+
+			var opts = {
+				type: 'confirm',
+				name: 'val',
+				message: 'Are you sure?'
+			};
+
+			if(typeof options === 'string'){
+				opts.message = options;
+			}
+
+			if(typeof options !== 'object'){
+				options = {};
+			}
+
+			opts.default = options.default || false;
+
+			inq.prompt([opts], function(res){
+
+				if(res.val){
+					cb(null, file);
+				}
+
+			});
+		});
+	}
 };

0eba87e38aecdc2a2e0d8c84d2b1b6ce35253546
Shannon Lal <shannonlal@hotmail.com>
2017-09-23 15:51:18 -0400

    Updating unit tests

--- test/gulp-prompt-spec.js
+++ test/gulp-prompt-spec.js
@@ -3,11 +3,11 @@ var assert = require('assert');
 var proxyrequire = require('proxyquire');
 var gulpPrompt = proxyrequire('../index.js', {'inquirer':{}, 'event-stream':{}});
 /**
- * 
+ * Simple test to ensure that the functions are still defined on main module
  */
 describe('gulp prompt unit tests', function() {
   describe('verify that the gulp prompt exists', function() {
-    it('Should verify that the package exists', function(){
+    it('Should verify that the module exists', function(){
       assert.notEqual(gulpPrompt, undefined);
     });
     it('Should verify that the package contains a function prompt', function(){
@@ -18,11 +18,3 @@ describe('gulp prompt unit tests', function() {
     });
   });
 });
-
-
-//Tests.  Makes sure you only prompt the user once.
-
-//Tests.  Makes sure that if option is string it converts it to message
-//Tests. Makes sure that if default is used, it is used.  If no default then sets
-//Tests.  Makes sure that if no options passed.  option is created 
-//Tests. That if multiple streams are passed it is only called once.  prompt should be called only once

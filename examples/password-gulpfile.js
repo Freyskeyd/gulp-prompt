81e745a85d50f3cda54acb919c88791c639e5e97
Shannon Lal <shannonlal@hotmail.com>
2017-09-25 20:28:36 -0400

    Increment version number

--- examples/password-gulpfile.js
+++ examples/password-gulpfile.js
@@ -4,10 +4,11 @@ var gulp = require( 'gulp' );
 var prompt = require('../index');
 
 /**
- * The following is a sample gulp file for getting the password from the command prompt
+ * The following is a sample gulp file for getting the list selection size from the command prompt
  * Note: Gulp Prompt is a wrapper around inquirer.js so if it is available in inquirer.js 
  * it should be available within gulp-prompt.  The prompt function is essentially a straight
- * passthrough to the inquirer.js library.  Here are the specs on how to configure the password:
+ * passthrough to the inquirer.js library.  The page size variable is what sets the length of display
+ * items in list
  * 
  * https://github.com/SBoudrias/Inquirer.js#questions
  */
@@ -17,20 +18,8 @@ gulp.task( 'getSelection',  () => {
             type:'list',
             name:'env',
             message:'Please enter selection?',
-            choices: ['a','b','c'.'d','e','f', 'g', 'h'],
+            choices: ['a','b','c','d','e','f', 'g', 'h'],
             pageSize:'3'
-        }, (res) => {
-            console.log('Result', res);
-        }) );
-});
-
-gulp.task( 'getPasswordAndValidate',  () => {
-    return gulp.src( './package.json' )
-        .pipe( prompt.prompt({
-            type:'password',
-            name:'env',
-            message:'Please enter password?',
-            validate: 
         }, (res) => {
             console.log('Result', res);
         }) );
